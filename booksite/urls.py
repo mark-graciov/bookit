ecce15f103b51ece25f33490af5adaa666017a86
Eliza <elizabet.galaju@gmail.com>
2016-06-08 13:01:43 +0300

    Rename links from *book* to *tale*

--- booksite/urls.py
+++ booksite/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.tale_list, name='tale_list'),
-	url(r'^create-book/(?P<tale_id>[0-9]+)$', views.create_book, name='create_book'),
+	url(r'^create-tale/(?P<tale_id>[0-9]+)$', views.create_tale, name='create_tale'),
 ]

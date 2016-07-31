3b1afc40b9878b9fa438ab005cc12b54865e81f1
ElizabetG <elizabet.galaju@gmail.com>
2016-07-31 12:34:14 +0300

    ga

--- bookit/settings.py
+++ bookit/settings.py
@@ -126,6 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (

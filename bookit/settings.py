dd5f9fc4d2fb459bb30b163b23227ca793785a70
ElizabetG <elizabet.galaju@gmail.com>
2016-07-31 23:34:07 +0300

    additional files and changes for Heroku

--- bookit/settings.py
+++ bookit/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bookit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bookit',
+        'USER': 'eliza',
+        'PASSWORD': 'alliance',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -135,3 +139,18 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
